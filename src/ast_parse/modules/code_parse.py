8c7bc6230f2d6bc3999bc2a7f0c8e785735c70b7
ZY-NULL <rayumond@live.com>
2026-04-07 20:52:35 +0800

    collect function definition from prj

--- src/ast_parse/modules/code_parse.py
+++ src/ast_parse/modules/code_parse.py
@@ -1,35 +1,47 @@
 from ast_parse.public import *
 from tree_sitter import Tree, Node
 from pathlib import Path
+from typing import Callable
 
 
-def collect_symbol_impl_from_prj(symbol: str, prj_path: Path):
+def collect_func_definition_from_prj(symbol: str, prj_path: Path):
     code_files: list[Path] = collect_code_files_from_prj(prj_path=prj_path)
     filtered_files: list[Path] = is_content_in_files(file_paths=code_files, search_content=symbol, whole_word=True, case_sensitive=True, is_regex=False)
+    target_types: list[str] = ["function_definition"]
     target_nodes: list[TsNodeView] = [
         TsNodeView.from_node(node, with_children=False, src=str(file))
         for file in filtered_files
-            for node in get_symbol_nodes_from_file(symbol=symbol, file=file)
+            for node in _get_symbol_nodes_from_file(symbol=symbol, file=file, target_node_types=target_types)
         ]
     # contents: list[str] = [n.text for n in target_nodes]
     return target_nodes
 
-def get_symbol_nodes_from_file(symbol: str, file: Path) -> list[Node]:
+def _get_symbol_nodes_from_file(symbol: str, file: Path, target_node_types: list[str]|None = None) -> list[Node]:
     t: Tree = parse_code_file(file=file)
     root = t.root_node
-    target_nodes = find_node_by_name(symbol_name=symbol, cur_node=root)
+    target_nodes = _find_node_by_name(symbol_name=symbol, root_node=root, target_node_types=target_node_types)
     return target_nodes
 
-def find_node_by_name(symbol_name: str, cur_node: Node)->list[Node]:
-    viewer = TsNodeView.from_node(node=cur_node, with_children=False)
-    full_id = viewer.node_id.split("::")
-    if all(sub_symbol in full_id for sub_symbol in symbol_name.split("::")):
-        return [cur_node]
+def _find_node_by_name(symbol_name: str, root_node: Node, target_node_types: list[str]|None = None)->list[Node]:
+    _is_type_match = lambda t: True
+    if target_node_types:
+        _is_type_match = lambda t: t in target_node_types
 
-    result = []
-    for child in cur_node.children:
-        result.extend(find_node_by_name(symbol_name=symbol_name, cur_node=child))
+    def _find_node_impl(target_name: list[str], cur_node: Node, type_matcher: Callable[[str], bool])->list[Node]:
+        if type_matcher(cur_node.type):
+            id_getter = TSNodeId(node=cur_node)
+            cur_ids = id_getter.node_id_list
+            match_res = match_name_list(target_name, cur_ids)
+            if match_res != 0:
+                return [cur_node]
+        match_result: list[Node] = []
+        for child in cur_node.children:
+            match_result.extend(_find_node_impl(target_name=target_name, cur_node=child, type_matcher=type_matcher))
+        return match_result
+    
+    result = _find_node_impl(target_name=symbol_name.split("::"), cur_node=root_node, type_matcher=_is_type_match)
     return result
+
 
 def collect_code_files_from_prj(prj_path: Path) -> list[Path]:
     files: list[Path] = []
--- test/test_get_symbol.py
+++ test/test_get_symbol.py
@@ -1,4 +1,4 @@
-from ast_parse.modules.code_parse import collect_symbol_impl_from_prj
+from ast_parse.modules.code_parse import collect_func_definition_from_prj
 from sys import argv as input_params
 from pathlib import Path
 
@@ -7,12 +7,13 @@ def test_local():
         return
     symbol = input_params[1]
     prj_dir = input_params[2]
-    result = collect_symbol_impl_from_prj(symbol=symbol, prj_path=Path(prj_dir))
+    result = collect_func_definition_from_prj(symbol=symbol, prj_path=Path(prj_dir))
     for res in result:
         print(f"{res.src}:{res.line}")
         print(res.node_type)
         print(res.text)
         print()
+    print(f"total {len(result)} result(s)")
 
 
 if __name__ == "__main__":
